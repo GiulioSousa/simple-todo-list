f4012fde0e06a95a04933d0a6b5fd54ec60a7550
Giulio Sousa <giulivan_cs@hotmail.com>
2023-09-19 21:19:54 -0300

    Correções na formatação de data e exclusão de tarefas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,42 @@
-const taskForm = document.getElementById('task-form');
-const taskList = document.getElementById('task-list');
+const taskForm = document.getElementById('task-form')
+const taskList = document.getElementById('task-list')
 
-taskForm.addEventListener('submit', function (e) {
-    e.preventDefault();
+taskForm.addEventListener('submit', e => {
+    e.preventDefault()
 
-    const date = document.getElementById('task-date').value;
-    const description = document.getElementById('task-description').value;
-    const status = document.getElementById('task-status').value;
+    const data = getFormData(taskForm)
+    const newRow = taskList.insertRow()
 
-    const newRow = taskList.insertRow();
     newRow.innerHTML = `
-                <td>${date}</td>
-                <td>${description}</td>
-                <td>${status}</td>
+                <td>${data.date}</td>
+                <td>${data.description}</td>
+                <td>${data.status}</td>
                 <td>
                     <button class="btn-delete" onclick="deleteTask(this)">Excluir</button>
                 </td>
-            `;
+            `
+    taskForm.date.value = ''
+    taskForm.description.value = ''
+    taskForm.status.value = 'Em Andamento'
+}) 
 
-    document.getElementById('task-date').value = '';
-    document.getElementById('task-description').value = '';
-    document.getElementById('task-status').value = 'Em Andamento';
-});
+function getFormData(formData) {
+    let task = {
+        date: formatDate(formData.date.value),
+        description: formData.description.value,
+        status: formData.status.value
+    }
+
+    return task
+}
+
+function formatDate (date) {
+    const arrayDate = date.split('-')
+    const formattedDate = `${arrayDate[2]}-${arrayDate[1]}-${arrayDate[0]}`
+    return formattedDate
+}
 
 function deleteTask(button) {
-    const row = button.parentNode.parentNode;
-    taskList.deleteRow(row.rowIndex);
+    const row = button.parentNode.parentNode
+    return taskList.deleteRow(row.rowIndex - 1)
 }
